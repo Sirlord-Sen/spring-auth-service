19566135d2fef27668a3fce4acdb493972fe9535
Sir-Lord <60177652+Sirlord-Sen@users.noreply.github.com>
2022-03-09 18:56:00 -0600

    setup: dtos

--- src/main/java/com/example/springauthservice/auth/dto/AuthDto.java
+++ src/main/java/com/example/springauthservice/auth/dto/AuthDto.java
@@ -1,5 +1,16 @@
 package com.example.springauthservice.auth.dto;
 
 public class AuthDto {
-    
+    public class LoginDto{
+        private String email;
+        private String password;
+
+        public String getEmail(){
+            return email;
+        }
+
+        public String getPassword(){
+            return password;
+        }
+    }
 }
--- src/main/java/com/example/springauthservice/user/dto/UserDto.java
+++ src/main/java/com/example/springauthservice/user/dto/UserDto.java
@@ -1,5 +1,35 @@
 package com.example.springauthservice.user.dto;
 
 public class UserDto {
+    public class CreateUserDto{
+        private String username;
+        private String email;
+        private String firstname;
+        private String surname;
+        private String password;
+
+        public String getUsername(){
+            return username;
+        }
+
+        public String getEmail(){
+            return email;
+        }
+
+        public String getFirstname(){
+            return firstname;
+        }
+
+        public String getSurname(){
+            return surname;
+        }
+
+        public String getPassword(){
+            return password;
+        }
+    }
+
     
 }
+
+
